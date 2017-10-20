707265b0dbc8c44b98f3bed2068d72e1cd6bb59b
Starry Zhang <32829409+StrayStarry@users.noreply.github.com>
2017-10-20 14:27:22 +0800

    Create dec_func.h

---
+++ dec_func.h
@@ -0,0 +1,22 @@
+/*
+** 2001 September 15
+**
+** The author disclaims copyright to this source code.  In place of
+** a legal notice, here is a blessing:
+**
+**    May you do good and not evil.
+**    May you find forgiveness for yourself and forgive others.
+**    May you share freely, never taking more than you give.
+****************************************************************
+**
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "sqlite3.h"
+
+int main(int agrc, const char *argv[])
+{
+	;
+	return 0;
+}
